f7e68c5e13614d042e676f6e6de5318afd280577
Zumatramo <50429076+Zumatramo@users.noreply.github.com>
2025-05-17 18:43:40 +0200

    added landing page
    landing page + routing updated

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,13 +4,13 @@ import { LoginComponent } from './login.component';
 import { ProfileComponent } from './profile.component';
 import { authGuard } from './auth.guard';
 import { AdminDashboardComponent } from './admin-dashboard.component';
-import { HomeComponent } from './home.component';
 import { TeacherDashboardComponent } from './teacher-dashboard.component';
 import { ExercisesComponent } from './exercises.component';
 import { TeacherExercisesComponent } from './teacher-exercises.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: LandingPageComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
